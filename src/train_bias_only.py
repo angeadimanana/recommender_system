7bb40964ec030d563b2ccf9fc4e33b77b5641098
angeadimanana <ange11notahin@gmail.com>
2025-11-21 09:49:13 +0200

    restructure and add split_train_test and train_bias_only

---
+++ src/train_bias_only.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+def update_bias(data_train_by_user, lamda, gamma, movie_biases):
+    m = len(data_train_by_user)
+    
+    biases = np.zeros(m)
+
+    for i in range(m):
+        bias = 0
+        count = 0
+        for (n,r) in data_train_by_user[i]:
+            bias += lamda * (r - movie_biases[n])
+            count += 1
+        bias = bias / (lamda * count + gamma)
+        biases[i] = bias
+    
+    return biases
+
+
+def cost_function(data_train, user_biases, movie_biases, lamda, gamma):
+    m = len(data_train)
+
+    loss_train = 0
+
+
+    for x in range(m):
+      for (n,r) in data_train[x]:
+        loss_train += (r - user_biases[x] - movie_biases[n])**2
+    
+    loss_train =  lamda * loss_train /2
+    loss_train = loss_train + gamma * np.sum(user_biases**2) / 2
+    loss_train = loss_train + gamma * np.sum(movie_biases**2) / 2
+
+    return loss_train
+
+def train(data_train_by_user, data_train_by_movie, data_test_by_user, data_test_by_movie, lamda, gamma, N):
+    
+    user_biases = np.zeros(len(data_train_by_user))
+    movie_biases = np.zeros(len(data_train_by_movie))
+    
+    costs_train = []
+    costs_test = []
+
+    for tmp in range(N) :
+        update_bias(data_train_by_user, lamda,gamma, movie_biases)
+        update_bias(data_train_by_movie, lamda, gamma, user_biases)
+
+        cout_train = cost_function(data_test_by_user, user_biases, movie_biases, lamda, gamma)
+        cout_test  = cost_function(data_test_by_user, user_biases, movie_biases, lamda, gamma) 
+        
+        costs_train.append(cout_train)
+        costs_test.append(cout_test)
+    
+    return user_biases, movie_biases, costs_train, costs_test
+
+
