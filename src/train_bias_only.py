45493bf89323053f0cde2fbf294baefe603dd8cf
angeadimanana <angenotahin@aims.ac.za>
2025-11-21 17:02:13 +0200

    modify upadate_bias function

--- src/train_bias_only.py
+++ src/train_bias_only.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-def update_bias(data_train_by_user, lamda, gamma, movie_biases):
+def update_bias(data_train_by_user, user_biases,movie_biases,lamda, gamma):
     m = len(data_train_by_user)
     
-    biases = np.zeros(m)
-
     for i in range(m):
         bias = 0
         count = 0
@@ -12,7 +10,7 @@ def update_bias(data_train_by_user, lamda, gamma, movie_biases):
             bias += lamda * (r - movie_biases[n])
             count += 1
         bias = bias / (lamda * count + gamma)
-        biases[i] = bias
+        user_biases[i] = bias
     
    
 
@@ -35,7 +33,7 @@ def cost_function(data_train, user_biases, movie_biases, lamda, gamma):
 
     return rmse, loss_train
 
-def train(data_train_by_user, data_train_by_movie, data_test_by_user, data_test_by_movie, lamda, gamma, N):
+def train(data_train_by_user, data_train_by_movie, data_test_by_user, lamda, gamma, N):
     
     user_biases = np.zeros(len(data_train_by_user))
     movie_biases = np.zeros(len(data_train_by_movie))
@@ -46,7 +44,7 @@ def train(data_train_by_user, data_train_by_movie, data_test_by_user, data_test_
     rmse_test_list = []
 
     for tmp in range(N) :
-        update_bias(data_train_by_user, lamda,gamma, movie_biases)
+        update_bias(data_train_by_user,  user_biases, movie_biases, lamda, gamma)
         
         update_bias(data_train_by_movie, lamda, gamma, user_biases)
 
